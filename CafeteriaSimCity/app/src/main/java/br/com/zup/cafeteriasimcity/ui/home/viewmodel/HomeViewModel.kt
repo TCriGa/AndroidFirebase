f0451ba20197caa10c7caa3849e1922278b2297e
TCriGa <thay.c.gayv@gmail.com>
2022-07-15 11:57:18 -0300

    Atualizar

--- CafeteriaSimCity/app/src/main/java/br/com/zup/cafeteriasimcity/ui/home/viewmodel/HomeViewModel.kt
+++ CafeteriaSimCity/app/src/main/java/br/com/zup/cafeteriasimcity/ui/home/viewmodel/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package br.com.zup.cafeteriasimcity.ui.home.viewmodel
 
+import android.net.Uri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -7,12 +8,14 @@ import androidx.lifecycle.viewModelScope
 import br.com.zup.cafeteriasimcity.data.datasource.remote.RetrofitService
 import br.com.zup.cafeteriasimcity.data.model.CoffeeResponse
 import br.com.zup.cafeteriasimcity.domain.repository.AuthenticationRepository
+import br.com.zup.cafeteriasimcity.domain.repository.FavoriteRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class HomeViewModel : ViewModel() {
     private val authenticationRepository = AuthenticationRepository()
+    private val favoriteRepository = FavoriteRepository()
 
     private val _coffeeResponse = MutableLiveData<CoffeeResponse>()
     val coffeeResponse: LiveData<CoffeeResponse> = _coffeeResponse
@@ -37,6 +40,26 @@ class HomeViewModel : ViewModel() {
                 _loading.value = false
             }
         }
+    }
+
+
+    fun saveImageFavorited() {
+        val image = _coffeeResponse.value?.arquivo.toString()
+        val imagePath = getImagePath()
+
+        favoriteRepository.databaseReference().child("$imagePath")
+            .setValue(image) { error, reference ->
+                if (error != null) {
+                    _message.value = error.message
+                }
+                _message.value = FAVORITE_MESSAGE
+            }
+    }
+
+    private fun getImagePath(): String? {
+        val image = _coffeeResponse.value?.arquivo.toString()
+        val uri: Uri = Uri.parse(image)
+        return uri.lastPathSegment?.replace(".jpg", "")
     }
 
     fun getUserName() = authenticationRepository.getNameUser()
